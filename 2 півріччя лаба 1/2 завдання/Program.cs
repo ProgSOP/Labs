1e0674e091a0f9fa322e0cae7d461487dda872f7
ProgSOP <mishanasik2003@gamil.com>
2024-03-03 21:11:52 +0200

    files added

---
+++ 2 півріччя лаба 1/2 завдання/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel;
+
+Console.WriteLine("Введiть цiле число n:");
+int n;
+
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out n))
+    {
+        if (n >= 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Число менше 1");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Введено некоректне значення. Будь ласка, введiть цiле число.");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
+
+Console.WriteLine("Dictionary keys:");
+    int numberOfKeys;
+    while (true)
+    {
+    if (int.TryParse(Console.ReadLine(), out numberOfKeys))
+    {
+        if ((numberOfKeys > 0) && (numberOfKeys > n))
+        {
+            break;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Number is less than 1 or less than n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Введено некоректне значення. Будь ласка, введiть цiле число.");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    }
+
+    Dictionary<int, string> dict = new Dictionary<int, string>();
+
+    for (int i = 1; i <= numberOfKeys; i++)
+    {
+        Console.Write($"Key {i}:  ");
+        dict.Add(i, Console.ReadLine());
+        
+
+    }
+
+    var sort = from i in dict
+               where i.Key >= n
+               select i;
+
+    Console.WriteLine("      ");
+    Console.WriteLine("Result");
+
+    foreach (var item in sort)
+    {
+        Console.WriteLine("Key: " + item.Key + "    Value:" + item.Value);
+    }
---
+++ 2 півріччя лаба 1/завдання 3/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Введiть кiлькiсть чисел n:");
+int n;
+
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out n))
+    {
+        if (n >= 3)
+        {
+            break;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Число менше 3");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Введено некоректне значення. Будь ласка, введiть цiле число.");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
+Dictionary<int,int> dic = new Dictionary<int,int>();
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"Введiть значення для ключа {i}: ");
+    if (int.TryParse(Console.ReadLine(), out int value))
+    {
+        dic.Add(i, value);
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Некоректне значення. Введiть цiле число.");
+        i--;
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
+
+Console.WriteLine("Result: ");
+
+var group = from item in dic
+            where item.Value > 0 
+            select item;
+
+foreach (var i in group)
+{
+    Console.WriteLine(i.Value);
+}
