eb0ddadf316a0dca62f627f25463254bbb953519
ProgSOP <162049123+ProgSOP@users.noreply.github.com>
2024-03-27 02:34:35 +0200

    Files to Laba 3 added

---
+++ Class1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semestr_2_Laba3
+{
+    public class RozkladItems
+    {
+
+        public string StartingTime { get; set; }
+        public string EndingTime { get; set; }
+        public string Place { get; set; }
+        public string Text { get; set; }
+
+
+
+        public void Write()
+        {
+            Console.WriteLine($"Даннi про урок:");
+
+            Console.WriteLine($"Початок: {StartingTime}");
+            Console.WriteLine($"Закiнчення: {EndingTime}");
+            Console.WriteLine($"Кабiнет: {Place}");
+            Console.WriteLine($"Коментар: {Text}");
+            Console.WriteLine("-----------");
+            Console.WriteLine("     ");
+        }
+
+
+    }
+
+
+    public class Rozklad
+    {
+        private List<RozkladItems> _RozkladItems;
+
+        public Rozklad()
+        {
+            _RozkladItems = new List<RozkladItems>();
+        }
+
+        public void AddItem()
+        {
+            RozkladItems item = new RozkladItems();
+            Console.WriteLine("Starting time");
+            item.StartingTime = Console.ReadLine();
+            Console.WriteLine("Ending time");
+            item.EndingTime = Console.ReadLine();
+            Console.WriteLine("Place ");
+            item.Place = Console.ReadLine();
+            Console.WriteLine("Text");
+            item.Text = Console.ReadLine();
+            _RozkladItems.Add(item);
+        }
+        public void Add(RozkladItems item)
+        {
+            _RozkladItems.Add(item);
+        }
+
+
+        public void Clear()
+        {
+            _RozkladItems.Clear();
+        }
+        public void Writer()
+        {
+            foreach (var item in _RozkladItems)
+            {
+                item.Write();
+            }
+        }
+        public void Replace(string startingtimE, string endingtime, string place, string text)
+        {
+            int i = 0;
+            for (i = 0; i < _RozkladItems.Count; i++)
+            {
+
+                if (startingtimE == _RozkladItems[i].StartingTime)
+                {
+                    _RozkladItems[i].EndingTime = endingtime;
+                    _RozkladItems[i].Place = place;
+                    _RozkladItems[i].Text = text;
+                    break;
+                }
+            }
+
+        }
+        public void ReplacePlace(string startingtimE, string endingtime, string place, string text)
+        {
+            int i = 0;
+            for (i = 0; i < _RozkladItems.Count; i++)
+            {
+
+                if (place == _RozkladItems[i].Place)
+                {
+                    _RozkladItems[i].StartingTime = startingtimE;
+                    _RozkladItems[i].EndingTime = endingtime;
+
+                    _RozkladItems[i].Text = text;
+                    break;
+                }
+            }
+
+        }
+
+        public void Compare(Rozklad rozklad1)
+        {
+            int i = 0;
+            for (i=0;i<_RozkladItems.Count;i++)
+            {
+                for (int j=0;j<rozklad1._RozkladItems.Count;j++)
+                {
+                    if ((_RozkladItems[i].StartingTime == rozklad1._RozkladItems[j].StartingTime) && (_RozkladItems[i].Place == rozklad1._RozkladItems[j].Place))
+                    {
+
+                        Console.WriteLine("\n"+ $"{j+1} Урок перетинаються");
+                        Console.WriteLine("-----------------");
+
+                    }
+                }
+            }
+         
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,222 @@
+﻿
+
+using semestr_2_Laba3;
+using System.Drawing;
+
+Rozklad rozklad = new Rozklad();
+Console.WriteLine("Create first Rozklad");
+
+Console.WriteLine("Create default RozkladItems? :)       1-yes");
+string choice1 = Console.ReadLine();
+if (choice1 == "1")
+{
+    RozkladItems items = new RozkladItems();
+
+    items.StartingTime = "8:30";
+    items.EndingTime = "9:30";
+    items.Place = "room 301";
+    items.Text = "First meeting";
+
+    RozkladItems items1 = new RozkladItems();
+
+    items1.StartingTime = "9:40";
+    items1.EndingTime = "10:30";
+    items1.Place = "room 309";
+    items1.Text = "Second meeting";
+
+    RozkladItems items2 = new RozkladItems();
+
+    items2.StartingTime = "10:40";
+    items2.EndingTime = "11:30";
+    items2.Place = "room 309";
+    items2.Text = "Homework";
+
+    rozklad.Add(items);
+    rozklad.Add(items1);
+    rozklad.Add(items2);
+   
+}
+
+
+
+
+while (true)
+{
+
+    Console.WriteLine("Pick metod");
+    Console.WriteLine("1-Add");
+    Console.WriteLine("2-Clear");
+    Console.WriteLine("3-Write rozklad");
+    Console.WriteLine("4-Replase rozklad item by StartingTime");
+    Console.WriteLine("5-Replase rozklad item by Place");
+    Console.WriteLine(" Write \"end\" to stop program");
+
+
+    string choise = Console.ReadLine();
+
+    if (choise == "1")
+    {
+        RozkladItems item = new RozkladItems();
+        rozklad.AddItem();
+    }
+    if (choise == "2")
+    {
+        rozklad.Clear();
+        Console.WriteLine("Rozklad cleard");
+        Console.WriteLine("       ");
+     
+    }
+    if (choise == "3")
+    {
+        rozklad.Writer();
+    }
+    if (choise == "4")
+    {
+        Console.WriteLine("Write StartingTime of the class you want to change");
+        string startingtimechange = Console.ReadLine();
+        Console.WriteLine("Write endingtime of the class you want to change");
+        string endingtimechange = Console.ReadLine();
+        Console.WriteLine("Write Place of the class you want to change");
+        string Placechange = Console.ReadLine();
+        Console.WriteLine("Write Text of the class you want to change");
+        string TextChange = Console.ReadLine();
+
+        rozklad.Replace(startingtimechange,endingtimechange,Placechange,TextChange);
+    }
+    if (choise == "5")
+    {
+        Console.WriteLine("Write Place of the class you want to change");
+        string Placechange1 = Console.ReadLine();
+        Console.WriteLine("Write endingtime of the class you want to change");
+        string endingtimechange1 = Console.ReadLine();
+    
+        Console.WriteLine("Write StartingTime of the class you want to change");
+        string startingtimechange1 = Console.ReadLine();
+        Console.WriteLine("Write Text of the class you want to change");
+        string TextChange1 = Console.ReadLine();
+        rozklad.ReplacePlace(startingtimechange1, endingtimechange1, Placechange1, TextChange1);
+    }
+    if (choise == "end")
+    {
+        break;
+    }
+
+}
+//------------------------------------------------------------------------------------//
+
+
+
+Rozklad rozklad1 = new Rozklad();
+Console.WriteLine("\n"+ "Create second Rozklad");
+Console.WriteLine("Create default RozkladItems? :)       1-yes");
+string choice2 = Console.ReadLine();
+if (choice2 == "1")
+{
+    RozkladItems items = new RozkladItems();
+
+    items.StartingTime = "8:30";
+    items.EndingTime = "9:30";
+    items.Place = "room 301";
+    items.Text = "First meeting";
+
+    RozkladItems items1 = new RozkladItems();
+
+    items1.StartingTime = "9:40";
+    items1.EndingTime = "10:30";
+    items1.Place = "room 309";
+    items1.Text = "Second meeting";
+
+    RozkladItems items2 = new RozkladItems();
+
+    items2.StartingTime = "10:40";
+    items2.EndingTime = "11:30";
+    items2.Place = "room 309";
+    items2.Text = "Homework";
+
+    rozklad1.Add(items);
+    rozklad1.Add(items1);
+    rozklad1.Add(items2);
+
+}
+
+
+
+
+while (true)
+{
+
+    Console.WriteLine("Pick metod");
+    Console.WriteLine("1-Add");
+    Console.WriteLine("2-Clear");
+    Console.WriteLine("3-Write rozklad");
+    Console.WriteLine("4-Replase rozklad item by StartingTime");
+    Console.WriteLine("5-Replase rozklad item by Place");
+    Console.WriteLine("6-Compare rozklads");
+    Console.WriteLine(" Write \"end\" to stop program");
+
+
+    string choise = Console.ReadLine();
+
+    if (choise == "1")
+    {
+        //RozkladItems item = new RozkladItems();
+        rozklad1.AddItem();
+    }
+    if (choise == "2")
+    {
+        rozklad1.Clear();
+        Console.WriteLine("Rozklad cleard");
+        Console.WriteLine("       ");
+
+    }
+    if (choise == "3")
+    {
+        rozklad1.Writer();
+    }
+    if (choise == "4")
+    {
+        Console.WriteLine("Write StartingTime of the class you want to change");
+        string startingtimechange = Console.ReadLine();
+        Console.WriteLine("Write endingtime of the class you want to change");
+        string endingtimechange = Console.ReadLine();
+        Console.WriteLine("Write Place of the class you want to change");
+        string Placechange = Console.ReadLine();
+        Console.WriteLine("Write Text of the class you want to change");
+        string TextChange = Console.ReadLine();
+
+        rozklad1.Replace(startingtimechange, endingtimechange, Placechange, TextChange);
+    }
+    if (choise == "5")
+    {
+        Console.WriteLine("Write Place of the class you want to change");
+        string Placechange1 = Console.ReadLine();
+        Console.WriteLine("Write endingtime of the class you want to change");
+        string endingtimechange1 = Console.ReadLine();
+
+        Console.WriteLine("Write StartingTime of the class you want to change");
+        string startingtimechange1 = Console.ReadLine();
+        Console.WriteLine("Write Text of the class you want to change");
+        string TextChange1 = Console.ReadLine();
+        rozklad1.ReplacePlace(startingtimechange1, endingtimechange1, Placechange1, TextChange1);
+
+    }
+
+    if (choise == "6")
+    {
+
+        Console.WriteLine("\n"+ "Порiвняння: ");
+        rozklad.Compare(rozklad1);
+
+        
+    }
+    if (choise == "end")
+    {
+        break;
+    }
+
+}
+
+
+
+
+
